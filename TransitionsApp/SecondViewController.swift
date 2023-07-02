e5b339539e8aa3136d1e2eac87d51992f6fe13b8
shunsaku <shunsaku.work@gmail.com>
2023-07-02 16:33:46 +0900

    アイテム繋ぎ込み

--- TransitionsApp/SecondViewController.swift
+++ TransitionsApp/SecondViewController.swift
@@ -8,7 +8,9 @@
 import Foundation
 import UIKit
 
-class SecondViewController: UIViewController {
+final class SecondViewController: UIViewController {
+    @IBOutlet private weak var colorButton: UIButton!
+    @IBOutlet private weak var closeButton: UIButton!
     
 }
 
