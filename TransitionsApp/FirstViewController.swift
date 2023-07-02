e57828705be292e2c2465c8a3821f5c8e6f1a5d9
shunsaku <shunsaku.work@gmail.com>
2023-07-02 16:40:04 +0900

    コメントアウト削除

--- TransitionsApp/FirstViewController.swift
+++ TransitionsApp/FirstViewController.swift
@@ -14,7 +14,7 @@ final class FirstViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
