c7eb67777847065734c0cfb0752d5d41f8072629
shunsaku21 <138352637+shunsaku21@users.noreply.github.com>
2023-07-02 16:11:07 +0900

    ファイル追加 (#1)

---
+++ TransitionsApp/SecondViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SecondViewController.swift
+//  TransitionsApp
+//
+//  Created by beck on 2023/07/02.
+//
+
+import Foundation
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+}
+
