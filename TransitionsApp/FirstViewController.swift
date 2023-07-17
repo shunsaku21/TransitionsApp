d9db9dd81125ca9e40b367ae341d7dd4e79e6de5
shunsaku21 <138352637+shunsaku21@users.noreply.github.com>
2023-07-17 15:22:32 +0900

    画面遷移の機能追加 (#3)
    * 画面遷移追加
    
    * ”カラーのボタン押すと黄色の背景に変わる”右上のボタンを押すと、その画面が閉じる機能を付ける”機能を追加
    
    * changeLabel()関数を追加

--- TransitionsApp/FirstViewController.swift
+++ TransitionsApp/FirstViewController.swift
@@ -9,14 +9,42 @@ import UIKit
 
 final class FirstViewController: UIViewController {
 
+    //  Outlet
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var nextButton: UIButton!
     
+    //  プロパティ
+    var nameLabel = "A画面"
+    var butoonName = "次の画面へ"
+    
+    //  ライフサイクル
     override func viewDidLoad() {
         super.viewDidLoad()
+        changeNameButton()
+        changeLabel()
+        
+        //  関数
+    }
+    func changeNameButton() {
+        nextButton.setTitle(butoonName, for: .normal)
+    }
+    func changeLabel(){
+        titleLabel.text = nameLabel
+        titleLabel.font = titleLabel.font.withSize(40)
+        titleLabel.textAlignment = NSTextAlignment.center
+    }
+    
+    @IBAction func nextButton(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "SecondStoryboard", bundle: nil)
+        let secondViewController = storyboard.instantiateViewController(withIdentifier: "SecondView") as! SecondViewController
+        self.present(secondViewController, animated: true)
+    }
         
     }
+    
 
 
-}
+
+
+//  Extension
 
