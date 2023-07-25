5b26e43dc5e95fffc8ce639ab421e11dbf08f8ef
shunsaku <shunsaku.work@gmail.com>
2023-07-25 23:16:33 +0900

    バック画面のカラー設定 指摘箇所の修正

--- TransitionsApp/SecondViewController.swift
+++ TransitionsApp/SecondViewController.swift
@@ -7,49 +7,14 @@
 
 import Foundation
 import UIKit
-
-final class SecondViewController: UIViewController {
-    @IBOutlet private weak var redButton: UIButton!
-    @IBOutlet private weak var closeButton: UIButton!
-    @IBOutlet weak var greenButton: UIButton!
-    @IBOutlet weak var blueButton: UIButton!
-    var redColorButton = "赤"
-    var buttonName = "戻る"
-    var blueColorButton = "青"
-    var greenColorButton = "緑"
+enum Colour{
+    case blue
+    case red
+    case green
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        secondchangeNameButton()
-        thirdchangeNameButton()
-        forcecangeNameButton()
-        fifthchangeNameButton()
-        let selectedColor: colour = .blue
-        self.view.backgroundColor = selectedColor.colorSetting
-    }
-    func secondchangeNameButton(){
-        redButton.setTitle(redColorButton, for: .normal)
-    }
-    func thirdchangeNameButton(){
-        closeButton.setTitle(buttonName, for: .normal)
-    }
-    func forcecangeNameButton(){
-        blueButton.setTitle(blueColorButton, for: .normal)
-    }
-    func fifthchangeNameButton(){
-        greenButton.setTitle(greenColorButton, for: .normal)
-    }
-    
-    @IBAction func secondButtonTapped(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
-    }
-}
-
-enum colour{
-    case blue,red,green
-    var colorSetting: UIColor{
+    var colorSetting: UIColor {
         switch self {
-        case.blue:
+        case .blue:
             return.blue
         case .red:
             return.red
@@ -59,3 +24,54 @@ enum colour{
     }
 }
 
+final class SecondViewController: UIViewController {
+    @IBOutlet private weak var redButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    @IBOutlet weak var cioseedButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    
+    var redColorButton = "赤"
+    var buttonName = "戻る"
+    var blueColorButton = "青"
+    var greenColorButton = "緑"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        redchangeNameButton()
+        closechangeNameButton()
+        bluecangeNameButton()
+        greenchangeNameButton()
+    }
+    func redchangeNameButton() {
+        redButton.setTitle(redColorButton, for: .normal)
+    }
+    
+    @IBAction func blueButton(_ sender: Any) {
+        let selectedColor: Colour = .blue
+                self.view.backgroundColor = selectedColor.colorSetting
+    }
+    
+    @IBAction func redButton(_ sender: Any) {
+        let selectedColor: Colour = .red
+                self.view.backgroundColor = selectedColor.colorSetting
+    }
+    
+    @IBAction func greenButton(_ sender: Any) {
+        let selectedColor: Colour = .green
+                self.view.backgroundColor = selectedColor.colorSetting
+    }
+    func closechangeNameButton() {
+        cioseedButton.setTitle(buttonName, for: .normal)
+    }
+    func bluecangeNameButton() {
+        blueButton.setTitle(blueColorButton, for: .normal)
+    }
+    func greenchangeNameButton() {
+        greenButton.setTitle(greenColorButton, for: .normal)
+    }
+    
+    @IBAction func closeButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
