1abc4c23e269032b32f59882d530fad9c7fc11cf
shunsaku21 <138352637+shunsaku21@users.noreply.github.com>
2023-08-20 22:30:54 +0900

    Selectcolor (#6)
    * enumとswitchを追加
    
    * バック画面のカラー設定
    指摘箇所の修正
    
    * 修正点の改善

--- TransitionsApp/SecondViewController.swift
+++ TransitionsApp/SecondViewController.swift
@@ -8,35 +8,69 @@
 import Foundation
 import UIKit
 
-final class SecondViewController: UIViewController {
-    @IBOutlet private weak var colorButton: UIButton!
-    @IBOutlet private weak var closeButton: UIButton!
+enum Colour{
+    case blue
+    case red
+    case green
     
-    var nameButton = "カラー"
-    var buttonName = "戻る"
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        secondchangeNameButton()
-        thirdchangeNameButton()
-        
-    }
-    func secondchangeNameButton(){
-        colorButton.setTitle(nameButton, for: .normal)
-    }
-    func thirdchangeNameButton(){
-        closeButton.setTitle(buttonName, for: .normal)
-    }
-
-    @IBAction func buttonTapped(_ sender: Any) {
-        view.backgroundColor = UIColor.yellow
-    }
-    
-    @IBAction func secondButtonTapped(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
+    var colorSetting: UIColor {
+        switch self {
+        case .blue:
+            return.blue
+        case .red:
+            return.red
+        case .green:
+            return.green
+        }
     }
 }
 
-
-
-
+final class SecondViewController: UIViewController {
+    @IBOutlet private weak var redButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    @IBOutlet weak var closedButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    
+    var redColorButton = "赤"
+    var buttonName = "戻る"
+    var blueColorButton = "青"
+    var greenColorButton = "緑"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        redchangeNameButton()
+        closechangeNameButton()
+        bluecangeNameButton()
+        greenchangeNameButton()
+    }
+    func redchangeNameButton() {
+        redButton.setTitle(redColorButton, for: .normal)
+    }
+    
+    @IBAction func blueButton(_ sender: Any) {
+        let selectedColor: Colour = .blue
+        self.view.backgroundColor = selectedColor.colorSetting
+    }
+    
+    @IBAction func redButton(_ sender: Any) {
+        let selectedColor: Colour = .red
+        self.view.backgroundColor = selectedColor.colorSetting
+    }
+    
+    @IBAction func greenButton(_ sender: Any) {
+        let selectedColor: Colour = .green
+        self.view.backgroundColor = selectedColor.colorSetting
+    }
+    @IBAction func closeButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    func closechangeNameButton() {
+        closedButton.setTitle(buttonName, for: .normal)
+    }
+    func bluecangeNameButton() {
+        blueButton.setTitle(blueColorButton, for: .normal)
+    }
+    func greenchangeNameButton() {
+        greenButton.setTitle(greenColorButton, for: .normal)
+    }
+}
